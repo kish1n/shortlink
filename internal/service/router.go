866dbc83375abdc0ba858a9ad0b8b3994e9f72e6
trpdjke <trpdjkework@gmail.com>
2024-07-23 15:21:33 +0300

    off /shortened & /db

--- internal/service/router.go
+++ internal/service/router.go
@@ -4,7 +4,6 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/kish1n/shortlink/internal/service/handlers"
 	"github.com/kish1n/shortlink/internal/service/helpers"
-	"github.com/kish1n/shortlink/internal/service/requests"
 	"gitlab.com/distributed_lab/ape"
 	"log"
 	"net/http"
@@ -22,9 +21,9 @@ func (s *service) router() (chi.Router, error) {
 	)
 
 	r.Route("/integrations/shortlink", func(r chi.Router) {
-		r.Get("/db", requests.DBHandler)
+		//r.Get("/db", requests.DBHandler)
 		r.Post("/add", handlers.GetShort)
-		r.Get("/{shortened}", requests.RedirectHandler)
+		//r.Get("/{shortened}", requests.RedirectHandler)
 	})
 
 	log.Println("Starting server on :8080")
